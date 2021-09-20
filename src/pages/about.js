b8c9bb45420165d671c4755a979f451d6d9ac831
keval154 <ksorathiya890@rku.ac.in>
2021-09-20 18:21:27 +0530

    six tutorial complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const About = () => {
     return (
-        <Layout pageTitle="About page">
+      <Layout pageTitle="About page">
         <p>I am making this by following the gatsby tutorial. About page</p>
       </Layout>
     )
